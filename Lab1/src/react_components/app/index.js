630ff05537dee6b2472fcfee6223a012c4e9ab0f
AKremzer <75171579+AKremzer@users.noreply.github.com>
2023-03-06 20:46:37 +0700

    Удалены лишние детали, которые реализуются в других компонентах

--- Lab1/src/react_components/app/index.js
+++ Lab1/src/react_components/app/index.js
@@ -1,30 +1,26 @@
 import moment from 'moment'; // библиотека moment для работы с временем и датами
                              // для установки зайди в терминале в директорию со своим проектом и 
                              // npm install moment --save
+import styled from "styled-components"; 
 import {CalendarHeader} from "../calendar-header"; // см. файлы в react-components
 import {CalendarTitle} from "../calendar-title";
 import {CalendarGrid} from "../calendar-grid";
 
+const CalendarStyle = styled.div`
+    border-radius: 10px;
+    overflow: hidden;
+    box-shadow: 0 0 20px #ccc;
+`
 function App() {
-    //window.currentDay = moment; // сегодняшний день
     moment.updateLocale("en", {week: {dow: 1}}); // локаль надо менять, потому что здесь неделя начинается с воскресенья
-    let pageFirstDay = moment().startOf("month").startOf("week"); // первый день недели, на которой начинается месяц
-    let pageLastDay = moment().endOf("month").endOf("week");      // последний день недели, на которой кончается месяц
-    
-    let calendarSheet = [];
-    let calendarDay = moment(pageFirstDay);
-    while(!calendarDay.isAfter(pageLastDay)) {   // прибавляем дни к самому левому дню на странице календаря
-        calendarSheet.push(moment(calendarDay)); // и заносим в массив, пока заносимый элемент не станет больше
-        calendarDay.add(1, "day");               // самого правого дня на странице календаря
-    }
-    // везде, где используется moment с объектом moment в конструкторе, создается новый объект для того, чтобы не изменять старые объекты
+    let pageFirstDay = moment().startOf("month").startOf("week");
     
   return (
-    <div>
+    <CalendarStyle>
       <CalendarHeader />
       <CalendarTitle />
       <CalendarGrid pageFirstDay = {pageFirstDay}/>
-    </div>
+    </CalendarStyle>
   );
 }
 
