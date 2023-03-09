f55f059a25ba839ad12821310ae6129127844812
AKremzer <75171579+AKremzer@users.noreply.github.com>
2023-03-09 15:05:58 +0700

    Add files via upload

---
+++ Lab1/src/react_components/calendar-title/calendar-title-styles.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const MainDivStyle = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background-color: #F8F8FF;
+  padding: 15px;
+`
+
+export const TextStyle = styled.span`
+  font-size: 230%;
+`
+
+export const ButtonStyle = styled.button`
+  border: 0;
+  height: 5vh;
+  border-radius: 5px;
+  margin-top: 10%;
+  background-color: #dfdfe6;
+  padding-right: 10px;
+  padding-left: 10px;
+  outline: unset;
+  cursor: pointer;
+`
