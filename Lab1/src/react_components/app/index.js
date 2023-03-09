307c591684ec818d97c5c39270d8d3cf14dc9555
AKremzer <75171579+AKremzer@users.noreply.github.com>
2023-03-09 12:09:34 +0700

    Исправлено перелистывание страниц

--- Lab1/src/react_components/app/index.js
+++ Lab1/src/react_components/app/index.js
@@ -1,53 +1,53 @@
 import moment from 'moment'; // библиотека moment для работы с временем и датами
                              // для установки зайди в терминале в директорию со своим проектом и 
                              // npm install moment --save
-import styled from "styled-components"; 
+import styled from "styled-components";
 import {useEffect, useState} from "react";
 import {CalendarHeader} from "../calendar-header"; // см. файлы в react-components
 import {CalendarTitle} from "../calendar-title";
 import {CalendarGrid} from "../calendar-grid";
 
 const CalendarStyle = styled.div`
-    border-radius: 10px;
-    overflow: hidden;
-    box-shadow: 0 0 20px #ccc;
+  border-radius: 10px;
+  overflow: hidden;
+  box-shadow: 0 0 20px #ccc;
 `
 
 // положение формы, в которую записываются события
 const FormPosStyle = styled.div`
-    position: absolute;
-    z-index: 100;
-    background-color: rgba(0, 0, 0, 0.3);
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    top: 0;
-    bottom: 0;
-    right: 0;
-    left: 0;
+  position: absolute;
+  z-index: 100;
+  background-color: rgba(0, 0, 0, 0.3);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
 `
 // стиль формы
 const FormStyle = styled(CalendarStyle)`
-    width: 20vw;
-    background-color: aliceblue;
-    box-shadow: unset;
+  width: 20vw;
+  background-color: aliceblue;
+  box-shadow: unset;
 `
 // записи в календаре
 const EventStyle = styled.input`
-    padding: 5px 15px;
-    font-size: 85%;
-    width: 100%;
-    border: 0;
-    outline: 0;
-    border-bottom: 1px solid #c6c6cc;
-    background-color: aliceblue;
-    color: #c6c6cc;
+  padding: 5px 15px;
+  font-size: 85%;
+  width: 100%;
+  border: 0;
+  outline: 0;
+  border-bottom: 1px solid #c6c6cc;
+  background-color: aliceblue;
+  color: #c6c6cc;
 `
 
 const ButtonsWrap = styled.div`
-    margin-bottom: 5px;
-    margin-left: 1px;
-    margin-right: 1px;
+  margin-bottom: 5px;
+  margin-left: 1px;
+  margin-right: 1px;
 `
 
 // кнопки внутри формы
@@ -69,8 +69,14 @@ const ButtonsStyle = styled.button`
 // ссылка, по которой лежит сервер с нашими данными
 const jsonUrl = `http://localhost:3001`;
 function App() {
+    const [method, setMethod] = useState(null); // метод, выбранный в форме события (добавить новое событие или изменить старое)
+    const [event, setEvent] = useState(null); // выбранное событие для редактирования/добавления
+    const [isFormShowing, setShowForm] = useState(false); // определение того, нужно показывать форму для ввода событий или нет
+    const [events, setEvents] = useState([]); // события, которые отображаются на данной странице календаря
+    const [today, setToday] = useState(moment())
+
     moment.updateLocale("en", {week: {dow: 1}}); // локаль надо менять, потому что здесь неделя начинается с воскресенья
-    let pageFirstDay = moment().startOf("month").startOf("week");
+    let pageFirstDay = moment(today).startOf("month").startOf("week");
 
     // ниже: метка первого дня на странице календаря определенного месяца и последнего, чтобы выводить только события на этой странице
     const startDayFilter = moment(pageFirstDay).format('X');
@@ -85,11 +91,6 @@ function App() {
     // это хуки - они позволяют функциональному компоненту, которым является function App, иметь свое состояние
     // позже я попробую переписать этот компонент под классовый, чтобы он мог иметь состояние без этих конструкций
     // пока оставляю так, о хуках: https://reactjs.org/docs/hooks-intro.html, о компонентах: https://reactjs.org/docs/components-and-props.html
-    const [method, setMethod] = useState(null); // метод, выбранный в форме события (добавить новое событие или изменить старое)
-    const [event, setEvent] = useState(null); // выбранное событие для редактирования/добавления
-    const [isFormShowing, setShowForm] = useState(false); // определение того, нужно показывать форму для ввода событий или нет
-    const [events, setEvents] = useState([]); // события, которые отображаются на данной странице календаря
-    const [today, setToday] = useState(moment())
 
     // открытие формы ввода для выбранного события и действия (обновление или создание нового события)
     const openForm = (methodName, eventToUpdate) => {
@@ -146,44 +147,44 @@ function App() {
             cancelButton();
         })
     }
-    
+
     //перелистывание месяцев
     const prevPageHandler = () => setToday(prev => prev.clone().subtract(1,'month'));
     const todayPageHandler = () => setToday(moment());
     const nextPageHandler = () => setToday(prev => prev.clone().add(1,'month'))
-  return (
-      <>
-          {
-              isFormShowing ? (
-                  <FormPosStyle>
-                      <FormStyle>
-                          <EventStyle value = {event.title}
-                                      onChange = {e => changeEvent(e.target.value, "title")}
-                          />
-                          <EventStyle value = {event.description}
-                                     onChange = {e => changeEvent(e.target.value, "description")}
-                          />
-                          <ButtonsWrap>
-                              <ButtonsStyle onClick={cancelButton}>Cancel</ButtonsStyle>
-                              <ButtonsStyle onClick={eventHandler}>{method}</ButtonsStyle>
-                              <ButtonsStyle onClick={removeEventHandler}>Delete</ButtonsStyle>
-                          </ButtonsWrap>
-                      </FormStyle>
-                  </FormPosStyle>
-              ) : null
-          }
-          <CalendarStyle>
-              <CalendarHeader />
-              <CalendarTitle
-               today ={today}
-               prevPageHandler ={prevPageHandler}
-               todayPageHandler ={todayPageHandler}
-               nextPageHandler ={nextPageHandler}
+    return (
+        <>
+            {
+                isFormShowing ? (
+                    <FormPosStyle>
+                        <FormStyle>
+                            <EventStyle value = {event.title}
+                                        onChange = {e => changeEvent(e.target.value, "title")}
+                            />
+                            <EventStyle value = {event.description}
+                                        onChange = {e => changeEvent(e.target.value, "description")}
+                            />
+                            <ButtonsWrap>
+                                <ButtonsStyle onClick={cancelButton}>Cancel</ButtonsStyle>
+                                <ButtonsStyle onClick={eventHandler}>{method}</ButtonsStyle>
+                                <ButtonsStyle onClick={removeEventHandler}>Delete</ButtonsStyle>
+                            </ButtonsWrap>
+                        </FormStyle>
+                    </FormPosStyle>
+                ) : null
+            }
+            <CalendarStyle>
+                <CalendarHeader />
+                <CalendarTitle
+                    today ={today}
+                    prevPageHandler ={prevPageHandler}
+                    todayPageHandler ={todayPageHandler}
+                    nextPageHandler ={nextPageHandler}
                 />
-              <CalendarGrid pageFirstDay = {pageFirstDay} grid_events = {events} openForm = {openForm}/>
-          </CalendarStyle>
-      </>
-  );
+                <CalendarGrid pageFirstDay = {pageFirstDay} grid_events = {events} openForm = {openForm}/>
+            </CalendarStyle>
+        </>
+    );
 }
 
 export default App;
